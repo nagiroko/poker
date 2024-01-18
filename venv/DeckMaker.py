9b151f4ce7bf328b5191071646436f9252b32b83
nagiroko <hoobrajrohit@yahoo.com>
2024-01-17 21:04:47 -0500

    pading

--- venv/DeckMaker.py
+++ venv/DeckMaker.py
@@ -3,7 +3,7 @@ from CardCreator import card
 def Deck():
     Types = ["Hearts","Spades","Clubs","Diamonds"]
     Names = ["Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"]
-    Value = [0,2,3,4,5,6,7,8,9,10,10,10,10]
+    Value = [1,2,3,4,5,6,7,8,9,10,10,10,10]
     deck = list()
     for item in Types:
         for index, each in enumerate(Names):
